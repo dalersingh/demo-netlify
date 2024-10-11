24c8bac50999ac29eca4991cee5284f40852bb54
Daler Singh <dalersingh@live.com>
2024-10-10 21:19:41 -0400

    remove unwanted code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // Bringing in the required import from 'react-router-dom'
-import { Outlet } from 'react-router-dom';
-import Nav from './components/NavTabs';
+import { Outlet } from "react-router-dom";
+import Nav from "./components/NavTabs";
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
@@ -8,7 +8,7 @@ function App() {
     <>
       <Nav />
       <main className="mx-3">
-        <Outlet /> // home, about, contact
+        <Outlet />
       </main>
     </>
   );
